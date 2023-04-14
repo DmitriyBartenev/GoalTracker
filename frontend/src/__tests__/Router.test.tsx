d0011743e6840c039852b033d890fdd4ef727383
DmitriyBartenev <bartenevdmitriy@bk.ru>
2023-04-14 18:53:38 +0400

    Fixed tests

--- frontend/src/__tests__/Router.test.tsx
+++ frontend/src/__tests__/Router.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { renderWithRouter } from './helpers/renderWithRouter';
 
 describe('Router Test', () => {
